9463db006e8240f5266f080f99b557f10f625a9e
vamsimotupalli <vamsi.motupalli1@gmail.com>
2022-11-08 12:28:35 +0530

    initial commit

---
+++ scripts/deploy.py
@@ -0,0 +1,35 @@
+from brownie import FundMe, MockV3Aggregator, network, config, Contract
+from scripts.helpful_scripts import (
+    get_account,
+    deploy_mocks,
+    LOCAL_BLOCKCHAIN_ENVIRONMENTS,
+)
+
+
+def deploy_fund_me():
+    account = get_account()
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        price_feed_address = config["networks"][network.show_active()][
+            "eth_usd_price_feed"
+        ]
+        k= Contract.from_abi(
+            MockV3Aggregator._name, price_feed_address, MockV3Aggregator.abi
+        )
+        print(k.description())
+        print(f"fuck you {k.address}")
+        print(f"suck you {price_feed_address}")
+    else:
+        deploy_mocks()
+        price_feed_address = MockV3Aggregator[-1].address
+
+    fund_me = FundMe.deploy(
+        price_feed_address,
+        {"from": account},
+        publish_source=config["networks"][network.show_active()].get("verify")
+    )
+    print(f"Contract deployed to {fund_me.address}")
+    return fund_me
+
+
+def main():
+    deploy_fund_me()
